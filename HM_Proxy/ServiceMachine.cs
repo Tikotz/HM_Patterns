fd946e83a90f96f3c73c77993a6436c65a450160
ori tikozki <oritikozki@gmail.com>
2022-01-12 21:56:16 +0200

    Add project files.

---
+++ HM_Proxy/ServiceMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_Proxy
+{
+    public class ServiceMachine : IServiceMechine
+    {
+        private InteriorOffice interiorOffice;
+        public Client Client { get; set; }
+
+        public ServiceMachine(InteriorOffice interiorOffice)
+        {
+            this.interiorOffice = interiorOffice;
+        }
+
+        public void ChangeAddress()
+        {
+            Console.WriteLine("Address Updated ");
+        }
+
+        public void ChangePassword()
+        {
+            Console.WriteLine("Password changed");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"All Info...");
+        }
+
+        public void UpdatePassport()
+        {
+            Console.WriteLine("Enjoy New Passport");
+        }
+    }
+}
